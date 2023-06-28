e397065d4338bd1e9dc1b9da598b520d4e8c0282
B9nsai <l.stuetz98@gmail.com>
2023-06-28 12:34:25 +0200

    add: implemented logic

--- src/main/java/org/example/PalindromeNumber.java
+++ src/main/java/org/example/PalindromeNumber.java
@@ -1,4 +1,4 @@
-package org.example;
+ package org.example;
 
 /**
  * <h3>9. Palindrome Number</h3>
@@ -10,6 +10,14 @@ package org.example;
  */
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        return true;
+        if (x < 0) {
+            return false;
+        }
+        String chars = String.valueOf(x);
+        String firstHalf = chars.substring(0, chars.length()/2);
+        int middleIndex = chars.length() % 2 == 0 ? chars.length() / 2 : chars.length() / 2 + 1;
+        StringBuilder secondHalf = new StringBuilder(chars.substring(middleIndex)).reverse();
+
+        return firstHalf.contentEquals(secondHalf);
     }
 }
