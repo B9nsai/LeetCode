1def7044e2071a474beae2f871fb39e892ca5f48
B9nsai <l.stuetz98@gmail.com>
2023-06-28 12:12:04 +0200

    add: implemented method and test headers

---
+++ src/main/java/org/example/PalindromeNumber.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        return true;
+    }
+}
---
+++ src/test/java/PalindromeNumberTest.java
@@ -0,0 +1,21 @@
+import org.example.PalindromeNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PalindromeNumberTest {
+    private PalindromeNumber palindromeNumber;
+    @BeforeEach
+    void setup() {
+        palindromeNumber = new PalindromeNumber();
+    }
+
+    @Test
+    void shouldFindPalindrome() {
+
+    }
+
+    @Test
+    void shouldNotFindPalindrome() {
+
+    }
+}
