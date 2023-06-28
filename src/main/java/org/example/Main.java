e6ac839712b45d9f3f4991bea7a01adfc29892d9
B9nsai <l.stuetz98@gmail.com>
2023-06-28 11:03:24 +0200

    add: printArray, createTestArray

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,31 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        printArray(createTestArray());
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    private static void printArray(int[][] array) {
+        for (int[] i: array) {
+            for (int j: i) {
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
+
+    private static int[][] createTestArray() {
+        int[][] array = new int[3][3];
+
+        int count = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = count;
+                count++;
+            }
+        }
+
+        return array;
+    }
+
+
 }
