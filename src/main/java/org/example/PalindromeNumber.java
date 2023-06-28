fa005c9947ab9637108fb969c6a900d7b218dddc
B9nsai <l.stuetz98@gmail.com>
2023-06-28 12:20:07 +0200

    add: javadoc

--- src/main/java/org/example/PalindromeNumber.java
+++ src/main/java/org/example/PalindromeNumber.java
@@ -1,5 +1,13 @@
 package org.example;
 
+/**
+ * <h3>9. Palindrome Number</h3>
+ * Given an integer x, return true if x is a
+ * palindrome, and false otherwise.
+ *
+ * <h4>constraints</h4>
+ * -231 <= x <= 231 - 1
+ */
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         return true;
