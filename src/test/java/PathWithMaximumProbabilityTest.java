bb31b95a9a8e0f41beab20633d8969ec80c3cca4
B9nsai <l.stuetz98@gmail.com>
2023-06-28 14:52:37 +0200

    add: headers for test methods

---
+++ src/test/java/PathWithMaximumProbabilityTest.java
@@ -0,0 +1,28 @@
+import org.example.PathWithMaximumProbability;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PathWithMaximumProbabilityTest {
+    private PathWithMaximumProbability pathWithMaximumProbability;
+    @BeforeEach
+    void setup() {
+        pathWithMaximumProbability = new PathWithMaximumProbability();
+    }
+
+    @Test
+    void shouldFindProbabilityOnThreeEdges() {
+
+    }
+
+    void shouldFindProbabilityOnTwoEdges() {
+
+    }
+
+    void shouldFindProbabilityOnOneEdge() {
+
+    }
+
+    void shouldNotFindProbability() {
+        
+    }
+}
