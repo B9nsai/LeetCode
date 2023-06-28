4abc0ba17a5060e4e8a1f8bd7a8bcde2bc600686
B9nsai <l.stuetz98@gmail.com>
2023-06-28 11:24:32 +0200

    feat: implemented twoSum logic

--- src/main/java/org/example/TwoSum.java
+++ src/main/java/org/example/TwoSum.java
@@ -1,7 +1,16 @@
 package org.example;
 
 public class TwoSum {
+
     public int[] twoSum(int[] nums, int target) {
-        return null;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
     }
+
 }
