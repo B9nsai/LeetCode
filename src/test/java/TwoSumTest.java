3ba019cdacb96ccadd7e407f2f6d62d3c1c8d577
B9nsai <l.stuetz98@gmail.com>
2023-06-28 11:13:34 +0200

    add: test returning correct indices

--- src/test/java/TwoSumTest.java
+++ src/test/java/TwoSumTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class TwoSumTest {
+import org.example.TwoSum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTest {
+    private TwoSum twoSum;
+    @BeforeEach
+    void setup() {
+        twoSum = new TwoSum();
+    }
+    @Test
+    void shouldReturnCorrectIndices() {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] actual = twoSum.twoSum(nums, target);
+
+        int[] expected = {0, 1};
+        Assertions.assertEquals(expected, actual);
+    }
 }
