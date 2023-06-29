097c5df3979966aa3107b14dd70e36852a96549c
B9nsai <l.stuetz98@gmail.com>
2023-06-29 10:02:29 +0200

    add: fields are private, generated getters and setters

--- src/main/java/org/example/ListNode.java
+++ src/main/java/org/example/ListNode.java
@@ -8,8 +8,8 @@ package org.example;
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; } <br>
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    private int val;
+    private ListNode next;
     public ListNode() {
     };
 
@@ -18,6 +18,22 @@ public class ListNode {
     }
     public ListNode(int val, ListNode next) {
         this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
         this.next = next;
     }
 }
