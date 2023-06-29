c3b8c4c74f618e402900485ac372f1ce36ade462
B9nsai <l.stuetz98@gmail.com>
2023-06-29 09:12:20 +0200

    fix: improved javadoc

--- src/main/java/org/example/ListNode.java
+++ src/main/java/org/example/ListNode.java
@@ -1,13 +1,11 @@
 package org.example;
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * <h1>singly-linked list</h1>
+ *     int val; <br>
+ *     ListNode next; <br>
+ *     ListNode() {} <br>
+ *     ListNode(int val) { this.val = val; } <br>
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; } <br>
  */
 public class ListNode {
     int val;
