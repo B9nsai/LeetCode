082671c7a3baa04f6fa2e057a65dcb4af3353e99
Leon <leon.stuetz@bancos.com>
2023-07-03 15:30:34 +0200

    add: unit tests

--- src/test/java/LetterCombinationsOfAPhoneNumberTest.java
+++ src/test/java/LetterCombinationsOfAPhoneNumberTest.java
@@ -1,2 +1,38 @@
+import org.example.LetterCombinationsOfAPhoneNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LetterCombinationsOfAPhoneNumberTest {
+    LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber;
+    @BeforeEach
+    void setup() {
+        letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
+    }
+
+    @Test
+    void shouldAppendNoDigitsOnEmptyInput(){
+        String digits = "";
+
+        List<String> actual = letterCombinationsOfAPhoneNumber.letterCombinations(digits);
+        List<String> expected = new ArrayList<>();
+
+        Assertions.assertEquals(0, actual.size());
+        Assertions.assertTrue(expected.containsAll(actual));
+    }
+
+    @Test
+    void shouldAppendAllPossibleCombinations(){
+        String digits = "23";
+
+        List<String> actual = letterCombinationsOfAPhoneNumber.letterCombinations(digits);
+        List<String> expected = Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf");
+
+        Assertions.assertEquals(expected.size(), actual.size());
+        Assertions.assertTrue(expected.containsAll(actual));
+    }
 }
