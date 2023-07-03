447d2b18b88df5cf74691fe3b44a8321d13b4499
Leon <leon.stuetz@bancos.com>
2023-07-03 16:30:28 +0200

    add: link to original problem

--- src/main/java/org/example/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/org/example/LetterCombinationsOfAPhoneNumber.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 
 /**
- * <h1>17. Letter Combinations of a Phone Number</h1>
+ * <a href="https://leetcode.com/problems/letter-combinations-of-a-phone-number/"><h1>17. Letter Combinations of a Phone Number</h1></a>
  * <p>Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.</p>
  * <p>A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.</p>
  * <h2>example</h2>
@@ -40,6 +40,7 @@ public class LetterCombinationsOfAPhoneNumber {
             }
             return appendedLetterList;
         }
+
         for (String letters : letterList) {
             for (Character letter : phoneButtonMap.get(nextDigit)) {
                 appendedLetterList.add(letters + letter);
