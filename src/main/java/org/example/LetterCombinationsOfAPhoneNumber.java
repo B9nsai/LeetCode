74dc4fbf4e4ef9890bfdfb5eea4d48fcc1c53b4b
Leon <leon.stuetz@bancos.com>
2023-07-03 14:58:12 +0200

    add: implemented empty base class

---
+++ src/main/java/org/example/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * <h1>17. Letter Combinations of a Phone Number</h1>
+ * <p>Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.</p>
+ * <p>A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.</p>
+ * <h2>example</h2>
+ * <p>Input: digits = "23"</p>
+ * <p>Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]</p>
+ * <ul>
+ *     <h3>constraints</h3>
+ *     <li>0 <= digits.length <= 4</li>
+ *     <li>digits[i] is a digit in the range ['2', '9'].</li>
+ * </ul>
+ */
+public class LetterCombinationsOfAPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+        return new ArrayList<>();
+    }
+}
